c2ddf4b4cd761ae86a9dac534a0ef9dac6d21348
Vadim <vadim11041996@hotmail.com>
2018-03-12 20:51:09 +0200

    can get all the data we need, no CSS

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Form = props =>(
+  <form onSubmit={props.getWeather}>
+    <input type="text" name="city" placeholder="City..."/>
+    <input type="text" name="country" placeholder="Country..."/>
+    <button className="btn btn-primary">Get Weather</button>
+  </form>
+);
+
+export default Form;
