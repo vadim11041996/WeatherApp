7e9a76ab9cdbcf799ed4adda682e317a5816224e
Vadim <vadim11041996@hotmail.com>
2018-03-12 21:26:35 +0200

    added style to right col

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Titles = () =>(
   <div>
-    <h1>Weather Finder</h1>
+    <h1 className='title-container__title'>Weather Finder</h1>
     <p>Find Out Temperature,conditions and more...</p>
   </div>
 );
