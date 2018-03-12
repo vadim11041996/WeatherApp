4395d19563300191999c36c2eca2eab27a35714f
Vadim <vadim11041996@hotmail.com>
2018-03-12 19:14:41 +0200

    first commit of weather app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Titles from './components/Titles';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <Titles />
+
+      </div>
+    );
+  }
+};
+
+export default App;
